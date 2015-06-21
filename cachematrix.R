03a517792416172842c0778ccaee973b3084d90b
Yuri <j2me@tut.by>
2015-06-21 16:26:11 +0200

    Some clean-up for code and test cases

--- cachematrix.R
+++ cachematrix.R
@@ -14,13 +14,13 @@
 ##    getinverse: function to retreive cached computed inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-  if (!exists("cached_matrix")) cached_matrix <<- NULL
-  if (!exists("cached_inverse")) cached_inverse <<- NULL
+  cached_inverse <- NULL
+  
   set <- function(y) {
-    cached_matrix <<- y
+    x <<- y
     cached_inverse <<- NULL
   }
-  get <- function() cached_matrix
+  get <- function() x
   setinverse <- function(inverse) cached_inverse <<- inverse
   getinverse <- function() cached_inverse
   list(set = set, get = get,
@@ -35,21 +35,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  cache_m = makeCacheMatrix(x);
-  cached_x = cache_m$get();
+  cached_x = x$get();
   
   ## Check is matrix cached already
-  if (!is.null(cached_x) && identical(x, cached_x)) {
-    cached_inv <- cache_m$getinverse()
+  if (!is.null(cached_x)) {
+    cached_inv <- x$getinverse()
   
     if(!is.null(cached_inv)) {
-
       return(cached_inv)
     }
   }
   ## if it's not cached - set to x, compute inverse and cache
-  cache_m$set(x)
-  inv <- solve(x, ...)
-  cache_m$setinverse(inv)
+  inv <- solve(cached_x, ...)
+  x$setinverse(inv)
   inv
 }
--- cachematrix_tests.R
+++ cachematrix_tests.R
@@ -5,50 +5,51 @@ library(RUnit)
 
 source("cachematrix.R")
 
-
 .setUp <- function() {
-  if (exists("cached_matrix")) rm(cached_matrix, inherits = TRUE)
   if (exists("cached_inverse")) rm(cached_inverse, inherits = TRUE)
   testMatrix0 <<- matrix(c(1,2,3,0,1,4,5,6,0),3,3)
+  testMatrix2 <<-  matrix(c(1, 2, 3, 4, 5, 6, 7, 8, 0),3,3)
 }
 
 test.cache <- function()
 {
   testMatrix1 <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8, 0),3,3)
-  testMatrix2 <-  matrix(c(1, 2, 3, 4, 5, 6, 7, 8, 0),3,3)
   
-  test1 <- cacheSolve(testMatrix1)
+  tm1 <- makeCacheMatrix(testMatrix1)
+  test1 <- cacheSolve(tm1)
   ## Check that result is not empty
   checkTrue(!is.null(test1))
   ## Check that matrix is returned 
   checkEquals(class(test1), "matrix")
   
-  test2 <- cacheSolve(testMatrix1)
+  test2 <- cacheSolve(tm1)
   checkIdentical(test1, test2)
   
-  test3 <- cacheSolve(testMatrix2)
+  test3 <- cacheSolve(makeCacheMatrix(testMatrix2))
   checkIdentical(test1, test3)
-  
-  ## Check that inverse is correct
-  checkEqualsNumeric(testMatrix2 %*% test3, diag(3))
-  
-  test4 <- cacheSolve(testMatrix0)
-  checkTrue(!identical(test4,test3))
 }
 
 test.inverse <- function() {
-
-  testInv <- cacheSolve(testMatrix0)
+  ## pre-defined example
+  testInv <- cacheSolve(makeCacheMatrix(testMatrix0))
   checkEquals(testInv[1,1], -24)
   testInvExpected <- matrix(sapply(c(-24, 18, 5, 20, -15, -4, -5, 4, 1), as.numeric),3,3);
   checkEqualsNumeric(testInvExpected, testInv)
+  
+
+  ## Check that general inverse is correct
+  test3 <- cacheSolve(makeCacheMatrix(testMatrix2))
+  checkEqualsNumeric(testMatrix2 %*% test3, diag(3))
+  
+  test4 <- cacheSolve(makeCacheMatrix(testMatrix0))
+  checkTrue(!identical(test4,test3))
 }
 
 test.errorHandling <- function() {
   testMatrixSingular <<- matrix(1:9, 3, 3)
-  checkException(cacheSolve('a'), 'Unable to take cacheSolve from wrong type')
-  checkException(cacheSolve(NULL), 'Unable to take cacheSolve from NULL')
-  checkException(cacheSolve(testMatrixSingular), 'Unable to take cacheSolve from singular')
+  checkException(cacheSolve(makeCacheMatrix('a')), 'Unable to take cacheSolve from wrong type')
+  checkException(cacheSolve(makeCacheMatrix(NULL)), 'Unable to take cacheSolve from NULL')
+  checkException(cacheSolve(makeCacheMatrix(testMatrixSingular)), 'Unable to take cacheSolve from singular')
 }
 
 runSuite <- function() {
